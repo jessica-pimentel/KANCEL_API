93c3c32722dc64deb9daa500bbdd7c6c27cbec3a
jessica-pimentel <jppimentel96@hotmail.com>
2024-07-27 11:26:40 -0300

    [CREATE] criado endpoints empty de applicationUser

--- BackEnd/api_kancel/Controller/Administration/ApplicationUserController.cs
+++ BackEnd/api_kancel/Controller/Administration/ApplicationUserController.cs
@@ -1,6 +1,32 @@
-﻿namespace api_kancel.Controller.Administration
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace api_kancel.Controller.Administration
 {
-    public class ApplicationUserController 
+    [ApiController]
+    [Route("[controller]")]
+    public class ApplicationUserController : ControllerBase
     {
+        public ApplicationUserController()
+        {
+            
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult> Login()
+        {
+            return Ok();
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult> Register()
+        {
+            return Ok();
+        }
+
+        [HttpPut("updatePassword")]
+        public async Task<ActionResult> UpdatePassword()
+        {
+            return Ok();
+        }
     }
 }
