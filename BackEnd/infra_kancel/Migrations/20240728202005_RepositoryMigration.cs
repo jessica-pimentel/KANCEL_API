4f3469d0eab2478fbf4b744961ba9a7e4b48ce94
jessica-pimentel <jppimentel96@hotmail.com>
2024-07-28 17:20:27 -0300

    [MIGRATION] nova migration para corrigir nome do banco

---
+++ BackEnd/infra_kancel/Migrations/20240728202005_RepositoryMigration.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace infra_kancel.Migrations
+{
+    /// <inheritdoc />
+    public partial class RepositoryMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_users",
+                table: "users");
+
+            migrationBuilder.RenameTable(
+                name: "users",
+                newName: "ApplicationUsers");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_ApplicationUsers",
+                table: "ApplicationUsers",
+                column: "ApplicationUserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_ApplicationUsers",
+                table: "ApplicationUsers");
+
+            migrationBuilder.RenameTable(
+                name: "ApplicationUsers",
+                newName: "users");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_users",
+                table: "users",
+                column: "ApplicationUserId");
+        }
+    }
+}
